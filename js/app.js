9650370d36d5f4f799d8a054181990cf9f3c9026
tmampa <tmampa94@gmail.com>
2023-11-10 10:56:06 +0200

    HTML and CSS code

---
+++ js/app.js
@@ -0,0 +1,13 @@
+function talk() {
+    var know = {
+        // key is the prompt from user, value is the response from chatbot
+
+        "Hello": "Hi there &#128540;",
+        "Ho are you": "My name is Chatty Betty.",
+        "What can you do for me": "Not much for now, just keeping you busy",
+        "Ok": "You're welcome",
+        "Bye": "Chat later, bye for now",
+        "Tell me about yourself": "I am just a smallnyana chat bot. Keeping you busy with HTML, CSS and JavaScript.",
+    };
+    console.log('Clicked')
+}
