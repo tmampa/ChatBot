51da3c7376b8a582a72919d2f72ee4e158b1d6d4
tmampa <tmampa94@gmail.com>
2023-11-10 12:57:31 +0200

    Configure string format

--- js/app.js
+++ js/app.js
@@ -2,12 +2,23 @@ function talk() {
     var know = {
         // key is the prompt from user, value is the response from chatbot
 
-        "Hello": "Hi there &#128540;",
-        "Ho are you": "My name is Chatty Betty.",
-        "What can you do for me": "Not much for now, just keeping you busy",
-        "Ok": "You're welcome",
-        "Bye": "Chat later, bye for now",
-        "Tell me about yourself": "I am just a smallnyana chat bot. Keeping you busy with HTML, CSS and JavaScript.",
+        "hello": "Hi there &#128540;",
+        "how are you": "My name is Chatty Betty.",
+        "what can you do for me": "Not much for now, just keeping you busy",
+        "ok": "You're welcome",
+        "bye": "Chat later, bye for now",
+        "tell me about yourself": "I am just a smallnyana chat bot. Keeping you busy with HTML, CSS and JavaScript.",
     };
-    console.log('Clicked')
+
+    var user = document.getElementById("userBox").value.toLowerCase().replace(/\?/g, "");
+    document.getElementById("chatLog").innerHTML = user + "<br>";
+
+    if (user in know) {
+        document.getElementById("chatLog").innerHTML = know[user] + "<br>";
+        
+    } else {
+        document.getElementById("chatLog").innerHTML = "Sorry I don't understand...<br>";
+    }
+    document.getElementById("userBox").value = "";
 }
+
